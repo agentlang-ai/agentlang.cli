050433162dd25efc3837f5053bd038aaeeb5b0e3
Shantanu Kumar <shantanu@fractl.io>
2024-08-14 17:41:12 +0530

    prepare to release with Docker image

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# Change Log
+All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+
+## [TODO]
+1. Support for Github based dependencies
+2. Support for multiple source paths (`:model-paths` in `model.fractl`)
+3. Resolver template, using `ftl new resolver <resolver-name>`
+
+## [TODO] [0.1.0] - 2024-08-??
+- Fetching Maven/Clojars dependencies
+  - `ftl deps`
+  - `ftl depstree`
+  - `ftl classpath`
+- Running Fractl app with dependencies
+  - `ftl run`
+- [WIP] Docker support
---
+++ docker-build.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+###
+## Script to build/push Docker image for fractl.cli
+##
+## ./docker-build.sh help
+
+set -e
+
+CTR_NAME="fractl.cli"
+IMG_NAME="fractl.cli"
+IMG_REPO="fractlio/fractl.cli"
+
+# Print to STDERR
+function err {
+  printf >&2 "$1 $2 $3 $4 $5 $6 $7 $8\n"
+}
+
+function getContainerId() {
+  docker ps -a | grep $CTR_NAME | tr -s ' ' | cut -d ' ' -f 1
+}
+
+function getImageId() {
+  docker images | grep $IMG_NAME | tr -s ' ' | cut -d ' ' -f 3
+}
+
+function clean() {
+  containerId=$(getContainerId)
+  if [ ! -z "$containerId" ]; then
+    docker kill $containerId || true
+    docker rm $containerId
+  fi
+
+  imageId=$(getImageId)
+  if [ ! -z "$imageId" ]; then
+    docker rmi $imageId
+  fi
+}
+
+function yesOrNo {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;
+            [Nn]*) err "Aborted" ; return  1 ;;
+        esac
+    done
+}
+
+function findVersion() {
+    cat project.clj | head --lines=1 | tr -s ' ' | cut -d ' ' -f 3 | tr -d '"'
+}
+
+function buildImage() {
+    lein do clean, test, uberjar
+    yesOrNo "Build local docker image $IMG_NAME" && \
+      docker build -f Dockerfile -t $IMG_NAME .
+}
+
+function pushToDockerHub() {
+    TAG_NAME="$1"
+    yesOrNo "Tag image as ${IMG_REPO}:${TAG_NAME}" && \
+      docker tag $IMG_NAME:latest $IMG_REPO:$TAG_NAME && \
+      yesOrNo "Push image ${IMG_REPO}:${TAG_NAME}" && \
+      docker login && \
+      echo docker push $IMG_REPO:$TAG_NAME
+}
+
+function pushVersion() {
+    pushToDockerHub "$(findVersion)"
+}
+
+function pushLatest() {
+    pushToDockerHub "latest"
+}
+
+case "$1" in
+build) buildImage;;
+clean) clean;;
+push-version) pushVersion;;
+push-latest) pushLatest;;
+*) err "Syntax: $0 build | clean | push-version | push-latest"
+esac
