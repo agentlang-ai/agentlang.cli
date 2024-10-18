cdbae4a7d63a8f650c2e4200102775da99dc705b
Shantanu Kumar <shantanu@fractl.io>
2024-10-18 09:46:19 +0530

    add LICENSE file to the tarball distribution

--- dist-build.sh
+++ dist-build.sh
@@ -17,7 +17,9 @@ function createUnix() {
   cp bin/agent $UNIX_DIST_HOME
   cp $UBERJAR_PATH $UNIX_DIST_HOME
   cp README.md $UNIX_DIST_HOME
+  cp LICENSE $UNIX_DIST_HOME
   tar czvf $UNIX_TARBALL_NAME -C $UNIX_DIST_BASE ${UNIX_DIST_NAME}
+  echo "Created Unix distribution: $UNIX_TARBALL_NAME"
 }
 
 createUnix
